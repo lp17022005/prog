752a4bc9ca87915e5f83fdedacbe99b162a309b3
Letícia Patricio <leticiapa2005@gmail.com>
2022-12-13 15:00:58 -0300

    correção urls

--- livraria/urls.py
+++ livraria/urls.py
@@ -2,28 +2,29 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
-from core.views import CategoriaViewSet, EditoraViewSet
-from uploader.router import router as uploader_router
+from core.views import (AutorViewSet, CategoriaViewSet, EditoraViewSet,
+                        LivroViewSet)
+from media.router import router as media_router
 
 router = DefaultRouter()
-router.register(r"categorias", CategoriaViewSet)
-router.register(r"editoras", EditoraViewSet)
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'editoras', EditoraViewSet)
+router.register(r'autores', AutorViewSet)
+router.register(r'livros', LivroViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-     path("api/", include(router.urls)),
+    path("api/", include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/media/", include(uploader_router.urls)),
+    path("api/media/", include(media_router.urls)),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger/",
@@ -35,8 +36,5 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-     
 ]
-
 urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
-
